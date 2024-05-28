b1a7c0a180737786030d07f2e761252e4fabe12a
patricia202222222222222 <166654962+patricia202222222222222@users.noreply.github.com>
2024-05-28 20:10:54 -0300

    first commit

---
+++ scrpit.js
@@ -0,0 +1,12 @@
+let nombre=prompt ("ingrese su nombre");
+function saludo(){
+    if(nombre){
+        alert("bienvenido " + nombre);
+    }
+        else {
+                alert("debe ingresar un nombre");
+            }
+        }
+
+
+saludo();
